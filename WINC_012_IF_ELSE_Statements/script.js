f2ebb619db8132ad016a892ff31dd0d53e4c0f63
Aarnoudse <83244265+Aarnoudse@users.noreply.github.com>
2021-05-31 20:39:36 +0200

    Add files via upload

---
+++ WINC_012_IF_ELSE_Statements/script.js
@@ -0,0 +1,24 @@
+const age = 3;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age > 18) {
+    console.log("You'are allowed to enter, have fun!")
+}
+else {
+    console.log("Stay out and please leave, thanks")
+}
+
+if (isFemale) {
+    console.log("Welcome to ladies night")
+}
+else {
+    console.log("See ya, wouldn't wanna be ya")
+}
+
+if (driverStatus == "bob") {
+    console.log("Drive safe")
+}
+else {
+    console.log("Go walking")
+}
